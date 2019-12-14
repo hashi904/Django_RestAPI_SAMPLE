a6b1f8c449d0327f7e0c73855bf98a07a5864e4d
mr904i <hashimoto_keisuke@hashimotokeiyuunoMacBook-puro.local>
2019-12-14 09:32:24 +0900

    JWTの導入

--- DataRecordApp/settings.py
+++ DataRecordApp/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'accounts',
     'blog',
     'rest_framework',
+    'rest_framework.authtoken', 
+    'rest_auth',           
 ]
 
 MIDDLEWARE = [
@@ -127,3 +129,16 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'accounts.User'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ),
+}
+
+REST_USE_JWT = True
