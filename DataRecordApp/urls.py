994242b42084b3a1b09317f399fcbb9c58befb36
mr904i <hashimoto_keisuke@hashimotokeiyuunoMacBook-puro.local>
2019-12-14 09:33:29 +0900

    login時にJWT発行
    /login/に接続時にJWTを発行するように設定した

--- DataRecordApp/urls.py
+++ DataRecordApp/urls.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url, include
-
+from rest_framework import routers
 from blog.urls import router as blog_router
 from accounts.urls import router as user_router
-
+from rest_framework_jwt.views import obtain_jwt_token
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^api/', include(user_router.urls)),
     url(r'^api/', include(blog_router.urls)),
+    url(r'^login/', obtain_jwt_token),
 ]
+
