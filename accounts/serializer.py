c23ba8852007e4721ae89055dbb6161b38c3e903
mr904i <hashimoto_keisuke@hashimotokeiyuunoMacBook-puro.local>
2019-12-14 10:41:03 +0900

    AuthRegisterSerializerの作成
    ユーザー作成時に入力情報をモデルに引き渡すserializer

--- accounts/serializer.py
+++ accounts/serializer.py
@@ -1,9 +1,20 @@
 from rest_framework import serializers
 
-from .models import User
+from .models import User, UserManager
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('id', 'username', 'date_of_birth', 'height', 'weight')
+
+
+class AuthRegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=False)
+
+    class Meta:
+        model = User
+        fields = ('id','email', 'username', 'date_of_birth', 'height', 'weight', 'password')
+
+    def create(self, validated_data):
+        return User.objects.create_user(request_data=validated_data)
