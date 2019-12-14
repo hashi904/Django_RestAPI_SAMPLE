afaf759cb85897b89b9dc9b608924c8c12c47c3c
mr904i <hashimoto_keisuke@hashimotokeiyuunoMacBook-puro.local>
2019-12-14 10:37:52 +0900

    Urls.pyの編集
    /api/signup => ユーザー新規登録
    /api/signin => ログイン
    とした

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,10 @@
+from django.conf.urls import url, include
 from rest_framework import routers
-from .views import UserViewSet
+from .views import UserViewSet, AuthRegister
 
 router = routers.DefaultRouter()
 router.register(r'user', UserViewSet)
+
+urlpatterns = [
+    url(r'signup/', AuthRegister.as_view()),
+]
