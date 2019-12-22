29e617d89d8e61297f9b44ad34d7b57a877c2480
mr904i <hashimoto_keisuke@hashimotokeiyuunoMacBook-puro.local>
2019-12-22 22:44:33 +0900

    Views.pyの作成
    作成した検索用のserializerをfilter_classにに定義し、検索することができるviewを作成した。

--- fitbit/views.py
+++ fitbit/views.py
@@ -3,9 +3,14 @@ from django.shortcuts import render
 from rest_framework import viewsets, filters
 
 from .models import HourSteps
-from .serializer import HourStepsSerializer
+from .serializer import HourStepsSerializer, HourStepsFilter
 
 
 class HourStepsViewSet(viewsets.ModelViewSet):
     queryset = HourSteps.objects.all()
     serializer_class = HourStepsSerializer
+
+class HourStepsSearchViewSet(viewsets.ModelViewSet):
+    queryset = HourSteps.objects.all()
+    serializer_class = HourStepsSerializer
+    filter_class = HourStepsFilter
