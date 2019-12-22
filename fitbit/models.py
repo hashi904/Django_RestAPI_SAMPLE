a0d152ba7c149ea3802730cde6a585fed4c26f64
mr904i <hashimoto_keisuke@hashimotokeiyuunoMacBook-puro.local>
2019-12-22 11:05:50 +0900

    string_dateカラム の作成
    クライアント側から日付で検索をかけるためにstrign_dateカラムを作成した

--- fitbit/models.py
+++ fitbit/models.py
@@ -3,4 +3,7 @@ from django.db import models
 class HourSteps(models.Model):
     hour_steps = models.IntegerField()
     time = models.DateTimeField(auto_now_add=True)
-
+    string_date = models.CharField(
+        verbose_name='string_date',
+        max_length=10,
+    )
