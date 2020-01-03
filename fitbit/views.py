aefccfe904b0d42f0b8ae43720e4f963d793ab81
mr904i <hashimoto_keisuke@hashimotokeiyuunoMacBook-puro.local>
2020-01-04 00:13:25 +0900

    検索fileterの作成
    string_dateカラムを完全一致検索するためにviews.pyを編集した。

--- fitbit/views.py
+++ fitbit/views.py
@@ -9,6 +9,12 @@ from .serializer import HourStepsSerializer, HourStepsFilter
 class HourStepsViewSet(viewsets.ModelViewSet):
     queryset = HourSteps.objects.all()
     serializer_class = HourStepsSerializer
+    def get_queryset(self):
+        queryset = HourSteps.objects.all()
+        string_date = self.request.query_params.get('string_date', None)
+        if string_date is not None:
+            queryset = queryset.filter(string_date=string_date)
+        return queryset
 
 class HourStepsSearchViewSet(viewsets.ModelViewSet):
     queryset = HourSteps.objects.all()
